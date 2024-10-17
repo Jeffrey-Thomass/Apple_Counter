9cd24b1ddc690422b1996f765c9c1c89ed4b7c89
Jeffry <jeffreythomas10566x@gmail.com>
2024-10-17 17:07:33 +0530

    Apple counter 2

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import leftArrow from './Assets/Images/left-arrow.png'
 import AppleCounter from './components/AppleCounter'
 export default function App() {
+  // this is comment
   return (
     <div>
        <AppleCounter />
